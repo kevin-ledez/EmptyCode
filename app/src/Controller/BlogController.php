7556fd0a6c8c29440700169a191fd6d2447d6fa9
kevin-ledez <kevin.ledez@gmail.com>
2024-02-11 22:47:17 +0100

    📦 NEW: Create Entity Article and Other

--- app/src/Controller/BlogController.php
+++ app/src/Controller/BlogController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class BlogController extends AbstractController
 {
     #[Route('/blog', name: 'app_blog')]
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
+        $articles = $articleRepository->findAll();
+
+        //dd($articles);
         return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+            'articles' => $articles,
+        ]);
+    }
+
+    #[Route('/blog/{slug}', name: 'app_blog_show')]
+    public function show(Article $article): Response
+    {
+        //dd($articles);
+        return $this->render('blog/show.html.twig', [
+            'article' => $article,
         ]);
     }
 }
